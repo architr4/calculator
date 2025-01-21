198ec989e132df980dc3c5f0659caaab0e6c229f
Archit Ranjan <142317686+architr4@users.noreply.github.com>
2025-01-21 13:15:03 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 //In case i mess up something const display = document.getElementById('display'); const buttons = document.querySelectorAll('.buttons button'); buttons.forEach(button => {button.addEventListener('click', () => {const value = button.dataset.value; if (value === '=') {try {display.value = eval(display.value);} catch (error) {display.value = 'Error';} } else { display.value += value;  } }); });
 
-
 const display = document.getElementById('display');
 const buttons = document.querySelectorAll('.buttons button');
 
